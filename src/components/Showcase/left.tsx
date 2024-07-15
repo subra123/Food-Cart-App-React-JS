da9ef5fd5d1063f67384f9ee661c56d98fe697da
subra123 <vishnusubramani8@gmail.com>
2024-07-15 20:17:08 +0530

    added template

---
+++ src/components/Showcase/left.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { BikeDelivery } from "../Assets";
+import { motion } from "framer-motion";
+const Left = () => {
+  return (
+    <div className="py-2 flex-1 flex flex-col items-start justify-center gap-3">
+      <div className="flex items-center gap-2 justify-center bg-orange-100 px-4 py-1 rounded-full">
+        <p className="text-base text-orange-500 font-bold">Super Fast Service with 0 wait time</p>
+        <div className="w-8 h-8 rounded-full overflow-hidden bg-white drop-shadow-xl">
+          <img
+            src={BikeDelivery}
+            alt="delivery"
+            className="w-full h-full object-contain"
+          />
+        </div>
+      </div>
+      <p className="text-[2rem] lg:text-[4rem] font-bold tracking-wide text-headingColor">
+      Experience Your Home Food Away From 
+        <span className="text-orange-600 text-[2.5rem] lg:text-[4.6rem]"> Home</span>
+      </p>
+      <p className="text-base text-textColor text-center md:text-left md:w-[80%]">
+      Welcome to our College Canteen Online Website! Enjoy super-fast service with zero wait time, bringing you your favorite meals without delays. Experience the comfort and taste of home-cooked food delivered right on campus.
+      </p>
+      <motion.button
+        whileHover={{ scale: 1.1 }}
+        className="bg-gradient-to-br from-orange-400 to-orange-500 w-full md:w-auto px-4 py-2 rounded-lg hover:shadow-lg transition-all ease-in-out duration-100"
+      >
+        Order Now
+      </motion.button>
+    </div>
+  );
+};
+
+export default Left;
---
+++ src/utils/showcaseStatic.tsx
@@ -0,0 +1,33 @@
+import {Chicken, Fish, Icecream, Strawberry,ChickenRice} from "../components/Assets";
+
+export const data = [
+
+    {
+        id: 2,
+        title: 'Panneer Rice',
+        desc: "Best Seller in Veg",
+        price: '80',
+        imgSrc: Strawberry
+    },
+    {   
+        id: 3,
+        title: 'Chicken Biriyani',
+        desc: "Best Seller in Non Veg",
+        price: '150',
+        imgSrc: Chicken
+    },
+    {
+        id: 4,
+        title: 'Chicken Rice',
+        desc: "Top Rated",
+        price: '110',
+        imgSrc: ChickenRice
+    },{
+        id: 1,
+        title: 'Gobi Chili',
+        desc: "Customer Choice",
+        price: '100',
+        imgSrc: Icecream
+    },
+
+]
